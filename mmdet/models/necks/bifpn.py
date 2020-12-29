4b58870687668effc0cd395862577464dc2ec70f
dqawami <daryan.qawami@intel.com>
2020-12-28 16:29:07 -0800

    Reverses fusion_conv and act_layer order for BiFPNNode

--- mmdet/models/necks/bifpn.py
+++ mmdet/models/necks/bifpn.py
@@ -14,24 +14,6 @@ from mmcv.cnn import normal_init, ConvModule
 # from mmcv.cnn.bricks import DepthwiseSeparableConvModule
 
 
-# temp until DepthwsieSeparableConvModule can be imported
-class SeparableConv2d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, **kwargs):
-        super().__init__()
-        self.depth_wise = nn.Conv2d(in_channels, in_channels, kernel_size=(kernel_size, kernel_size),
-                                    stride=(1, 1), padding=(1, 1), dilation=(1, 1), groups=in_channels)
-        self.point_wise = ConvModule(in_channels, out_channels, kernel_size=1, **kwargs)
-
-    def forward(self, x):
-        out = self.depth_wise(x)
-        out = self.point_wise(out)
-        return out
-
-    def init_weights(self):
-        normal_init(self.depth_wise, std=0.01)
-        normal_init(self.point_wise, std=0.01)
-
-
 class BiFPNNode(nn.Module):
     def __init__(self, input_channels, output_channel, num_backbone_features,
                  weight_method, act_fn, separable_conv, epsilon, input_offsets,
@@ -116,9 +98,9 @@ class BiFPNNode(nn.Module):
 
         nodes = torch.sum(nodes, dim=-1)
 
-        nodes = self.act_layer(nodes)
+        nodes = self.fusion_convs(nodes)
 
-        return self.fusion_convs(nodes)
+        return self.act_layer(nodes)
 
 
 class BiFPNBlock(nn.Module):

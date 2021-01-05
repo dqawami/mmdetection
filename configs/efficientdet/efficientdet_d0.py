54b3807768e7934745555b51db0ac4b8c6c447f5
dqawami <daryan.qawami@intel.com>
2021-01-04 20:42:46 -0800

    Added Exponential Moving Average

--- configs/efficientdet/efficientdet_d0.py
+++ configs/efficientdet/efficientdet_d0.py
@@ -136,6 +136,9 @@ data = dict(
 # optimizer
 optimizer = dict(type='SGD', lr=0.16, momentum=0.9, weight_decay=0.00004)
 optimizer_config = dict(grad_clip=dict(max_norm=10, norm_type=2))
+custom_hooks = [
+    dict(type='EMA', momentum=0.0001)
+]
 # learning policy
 lr_config = dict(
     policy='CosineRestart',

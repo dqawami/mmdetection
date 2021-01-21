bc718c3a0bfc8a6dd6295229b6e96d91ebbf031f
Qawami, Daryan <daryan.qawami@intel.com>
2021-01-21 03:07:12 +0000

    Made changes to EfficientDet-D0

--- configs/efficientdet/efficientdet_d0.py
+++ configs/efficientdet/efficientdet_d0.py
@@ -6,7 +6,7 @@ channels = 64
 num_levels = 5
 act_cfg = 'silu'
 separable_conv = True
-num_epochs = 300
+num_epochs = 70
 delta = 0.1
 model = dict(
     type='RetinaNet',
@@ -42,7 +42,7 @@ model = dict(
             type='AnchorGenerator',
             octave_base_scale=4,
             scales_per_octave=3,
-            ratios=[0.7, 1.0, 1.4],
+            ratios=[0.5, 1.0, 2.0],
             strides=[8, 16, 32, 64, 128]),
         bbox_coder=dict(
             type='DeltaXYWHBBoxCoder',
@@ -62,7 +62,7 @@ train_cfg = dict(
     assigner=dict(
         type='MaxIoUAssigner',
         pos_iou_thr=0.5,
-        neg_iou_thr=0.5,
+        neg_iou_thr=0.4,
         min_pos_iou=0,
         ignore_iof_thr=-1),
     allowed_border=-1,
@@ -72,7 +72,7 @@ test_cfg = dict(
     nms_pre=1000,
     min_bbox_size=0,
     score_thr=0.05,
-    nms=dict(type='soft_nms', iou_thr=0.5),
+    nms=dict(type='nms', iou_thr=0.5),
     max_per_img=100)
 # model training and testing settings
 # dataset settings
@@ -80,17 +80,17 @@ dataset_type = 'CocoDataset'
 data_root = './data/coco/'
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.374], to_rgb=True)
 train_pipeline = [
-    dict(type='LoadImageFromFile'),
+    dict(type='LoadImageFromFile', to_float32=True),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(
         type='Resize',
         img_scale=(input_size, input_size),
-        # ratio_range=(0.1, 2.0),
-        keep_ratio=True),
+        keep_ratio=True,
+        ratio_range=(0.1, 2.0)),
     dict(type='RandomCrop', crop_size=(input_size, input_size)),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size=(input_size, input_size)),
+    dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
 ]
@@ -104,23 +104,19 @@ test_pipeline = [
             dict(type='Resize', keep_ratio=False),
             dict(type='Normalize', **img_norm_cfg),
             dict(type='RandomFlip'),
+            dict(type='Pad', size_divisor=32),
             dict(type='ImageToTensor', keys=['img']),
             dict(type='Collect', keys=['img']),
         ])
 ]
 data = dict(
-    samples_per_gpu=20,
+    samples_per_gpu=18,
     workers_per_gpu=0,
     train=dict(
-        type='RepeatDataset',
-        times=1,
-        dataset=dict(
-            type=dataset_type,
-            ann_file=data_root + 'annotations/instances_train2017.json',
-            img_prefix=data_root + 'train2017',
-            pipeline=train_pipeline
-        )
-    ),
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_train2017.json',
+        img_prefix=data_root + 'train2017',
+        pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instances_val2017.json',
@@ -133,21 +129,22 @@ data = dict(
         img_prefix=data_root + 'val2017',
         test_mode=True,
         pipeline=test_pipeline))
+evaluation = dict(interval=1, metric='bbox')
 # optimizer
-optimizer = dict(type='SGD', lr=0.08, momentum=0.9, weight_decay=0.00004)
-optimizer_config = dict(grad_clip=dict(max_norm=10, norm_type=2))
+optimizer = dict(type='SGD', lr=0.08, momentum=0.9, weight_decay=4e-5)
+optimizer_config = dict(grad_clip=None)
 custom_hooks = [
-    dict(type='EMA', momentum=0.0001)
+    dict(type='EMA', momentum=0.0002)
 ]
 # learning policy
 lr_config = dict(
     policy='CosineRestart',
     periods=[num_epochs],
-    min_lr=0.00001,
+    min_lr=0,
     min_lr_ratio=None,
     warmup='linear',
     warmup_iters=1,
-    warmup_ratio=0.0001,
+    warmup_ratio=0.1,
     warmup_by_epoch=True
 )
 checkpoint_config = dict(interval=1)
